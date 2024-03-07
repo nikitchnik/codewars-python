022337832670463b1350c1c798d34a4eaaf77398
nikitchnik <113408818+nikitchnik@users.noreply.github.com>
2024-03-07 19:53:15 +0500

    Completed 4kyu-The_Greatest_Warrior.py

--- 4kyu-The_Greatest_Warrior.py
+++ 4kyu-The_Greatest_Warrior.py
@@ -1,68 +1,48 @@
 class Warrior:
     def __init__(self):
-        self.level = 1
-        self.experience = 100
-        self.require_experience = 200
-        self.rank = 'Pushover'
+        self._experience = 100
         self.achievements = []
-        
         self.ranks = ["Pushover", "Novice", "Fighter", "Warrior", "Veteran", "Sage", "Elite", "Conqueror", "Champion", "Master", "Greatest"]
-        
+
+
     def training(self, info):
         achieve = info[0]
         points = info[1]
         require_level = info[2]
         
-        if self.level < require_level:
-            return "Not strong enough"
+        if self.level < require_level: return "Not strong enough"
         
-        self.experience += points
+        self._experience += points
         self.achievements.append(achieve)
-        self.up_level()
         return achieve
-    
+
+
     def battle(self, level):
-        if level < 1 or level > 100:
-            return "Invalid level"
-        
-        enemy_rank = self.ranks[level // 10]
-        
-        if self.ranks.index(self.rank) < self.ranks.index(enemy_rank) and level - self.level > 5:
-            return "You've been defeated"
+        if level < 1 or level > 100: return "Invalid level"
+        elif self.level // 10 != level // 10 and self.level - level <= -5: return "You've been defeated"
         
         
-        if level == self.level:
-            points = 10
-            self.experience += points
-            self.up_level()
+        elif level == self.level:
+            self._experience += 10
             return "A good fight"
-            
         elif self.level - level == 1:
-            points = 5
-            self.experience += points
-            self.up_level()
+            self._experience += 5
             return "A good fight"
-        
-        elif self.level - level <= 2:
-            points = 0
+        elif self.level < level:
+            self._experience += 20 * ((level - self.level) ** 2)
             return "An intense fight"
-            
-        elif level - self.level >= 2:
-            points = 20 * (level - self.level) ** 2
-            if points == 0:
-                points = 20
-            self.experience += points
-            self.up_level()
+        else:
             return "Easy fight"
-        
-        
-    
-    def up_level(self):
-        while self.experience >= self.require_experience:
-            self.require_experience += 100
-            
-            self.level += 1
-            self.rank = self.ranks[self.level // 10]
-            if self.level >= 100:
-                self.level = 100
-                break
+
+
+    @property
+    def level(self):
+        return self.experience // 100
+
+    @property
+    def rank(self):
+        return self.ranks[self.experience // 1000]
+
+    @property
+    def experience(self):
+        return min(self._experience, 10000)
